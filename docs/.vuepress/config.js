ad786cdc0be591b5aa4f5be4773f3195c35f8371
Fosphatic-Duke <f.duke@web.de>
2019-03-02 04:09:02 +0100

    1.0.0

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -2,5 +2,13 @@ module.exports = {
   base: "/cards-theme-documentation/",
   title: 'October CMS Cards Theme Documentation',
   description: 'Just playing around',
-  dest: 'gh-pages'
+  dest: 'gh-pages',
+  themeConfig: {
+   nav: [
+     { text: 'Home', link: '/' },
+     { text: 'Guide', link: '/guide/' },
+     { text: 'External', link: 'https://google.com' },
+   ],
+   logo: '/assets/img/logo.svg',
+ }
 }
